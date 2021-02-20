2663c04bb9014bed5f02e4b7bbda5fee6f647ae2
Hamilton <josehamiltonsantosjunior@gmail.com>
2021-02-20 14:41:25 -0300

    coloquei img e fiz algumas pequenas modificaçoes

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+// Para poder recarregar sem precisar dar load
+
+// const shopCar = document.querySelector('')
+
+// shopCar.addEventListener('click', fetch('pagina1.html')
+//     .then(resposta => {
+//         // se por acaso a página não existir vai direto para o catch
+//         // lá eu posso tratar o erro 
+//             // jagando no console
+//             // ou direto na página 
+//             // como 404 por exemplo
+//         if(resposta.status !== 200) throw new Error ('ERRO 404 NOSSO')
+//         return resposta.text()
+//     })
+//     .then(html => console.log(html))
+//     .catch(err => console.error(err)))
